a865616d313fa8ffa5e81826cd68e97b4686ed87
Ishkina <102829553+Ishkina@users.noreply.github.com>
2022-04-08 23:24:22 +0300

    Add files via upload

---
+++ Dom_zadanie1.py
@@ -0,0 +1,16 @@
+print('Escape sequences')
+print('\\a\t\tBell (alter)')
+print('\\b\t\tBackspace')
+print('\\n\t\tNew line')
+print('\\t\t\tHorizontal tab')
+print('\\\\\t\tBackslash \\')
+print('\\\"\t\tDouble quotation mark \"')
+print('\\\'\t\tSingle quotation mark \'')
+
+
+
+
+
+
+
+
